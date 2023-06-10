9ebe4bc8e4c8c9c1052a2fb430c7ca68444a3b74
BathspaJohnAndreFederiso <71628636+BathspaJohnAndreFederiso@users.noreply.github.com>
2023-06-10 14:57:14 +0400

    backup of testfiles

--- testfiles/postsection.php
+++ testfiles/postsection.php
@@ -1,7 +1,35 @@
 <?php 
 
+// Update the details below with your MySQL details
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'mordhubdb';
+try {
+    $pdo = new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+} catch (PDOException $exception) {
+    // If there is an error with the connection, stop the script and display the error
+    exit('Failed to connect to database!');
+}
 
-include '../PHPOnly/connect.php';
+// Below function will convert datetime to time elapsed string
+function time_elapsed_string($datetime, $full = false) {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+    $string = array('y' => 'year', 'm' => 'month', 'w' => 'week', 'd' => 'day', 'h' => 'hour', 'i' => 'minute', 's' => 'second');
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
 
 
 // This function will populate the comments and comments replies using a loop
@@ -9,7 +37,7 @@ function show_comments($comments, $parent_id = -1) {
     $html = '';
     if ($parent_id != -1) {
         // If the comments are replies sort them by the "submit_date" column
-        array_multisort(array_column($comments, 'date'), SORT_ASC, $comments);
+        array_multisort(array_column($comments, 'submit_date'), SORT_ASC, $comments);
     }
     // Iterate the comments using the foreach loop
     foreach ($comments as $comment) {
@@ -19,7 +47,7 @@ function show_comments($comments, $parent_id = -1) {
             <div class="comment">
                 <div>
                     <h3 class="name">' . htmlspecialchars($comment['name'], ENT_QUOTES) . '</h3>
-                    <span class="date">' . time_elapsed_string($comment['date']) . '</span>
+                    <span class="date">' . time_elapsed_string($comment['submit_date']) . '</span>
                 </div>
                 <p class="content">' . nl2br(htmlspecialchars($comment['content'], ENT_QUOTES)) . '</p>
                 <a class="reply_comment_btn" href="#" data-comment-id="' . $comment['id'] . '">Reply</a>
@@ -55,12 +83,12 @@ if (isset($_GET['page_id'])) {
     // Check if the submitted form variables exist
     if (isset($_POST['name'], $_POST['content'])) {
         // POST variables exist, insert a new comment into the MySQL comments table (user submitted form)
-        $stmt = $pdo->prepare('INSERT INTO comments (page_id, parent_id, name, content, date) VALUES (?,?,?,?,NOW())');
+        $stmt = $pdo->prepare('INSERT INTO comments (page_id, parent_id, name, content, submit_date) VALUES (?,?,?,?,NOW())');
         $stmt->execute([ $_GET['page_id'], $_POST['parent_id'], $_POST['name'], $_POST['content'] ]);
         exit('Your comment has been submitted!');
     }
     // Get all comments by the Page ID ordered by the submit date
-    $stmt = $pdo->prepare('SELECT * FROM comments WHERE page_id = ? ORDER BY date DESC');
+    $stmt = $pdo->prepare('SELECT * FROM comments WHERE page_id = ? ORDER BY submit_date DESC');
     $stmt->execute([ $_GET['page_id'] ]);
     $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
     // Get the total number of comments
@@ -70,10 +98,8 @@ if (isset($_GET['page_id'])) {
 } else {
     exit('No page ID specified!');
 }
-
-
-
 ?>
+
 
 <div class="comment_header">
     <span class="total"><?=$comments_info['total_comments']?> comments</span>
